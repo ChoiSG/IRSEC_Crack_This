ace1c148ea35d92c228e2f492c59f9d4f5c23cc2
ChoI <36252854+ChoiSG@users.noreply.github.com>
2019-04-17 12:52:41 -0400

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,3 +1,4 @@
+# Little gift for Mr. Phi"ll"ip and window bois in IRSEC
 # Because fuck windows server's internet explorer complaining about adding the "webpage to trustworthy domain" or something 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
